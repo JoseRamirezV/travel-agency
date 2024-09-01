5adb379c3b23bd95ac6d225dd606e40205d4005c
jeison rodriguez <jeison953@gmail.com>
2024-09-01 17:17:29 -0500

    AGREGA funcinalidad reserva hoteles

---
+++ utils/reservations/vuelos.py
@@ -0,0 +1,5 @@
+vuelos = {
+   "1":  {"origen": "Cali(Colombia)", "destino": "Barcelona(españa)", "precio": 857},
+   "2": {"origen": "Cali(Colombia)", "destino": "cancun(mexico)", "precio": 343},
+   "3": {"origen": "Cali(Colombia)", "destino": "Londres(inglaterra)", "precio": 1100},
+}
